15f46fb4c0d1ceef06cd6646f7fb4d2095bdb80f
Fabio Albertin <albertin@gmail.com>
2017-09-01 00:15:12 +0100

    Excercises and README

--- 03-def-args/excercises.js
+++ 03-def-args/excercises.js
@@ -1,3 +1,5 @@
+/* 1 */
+
 function delayed(delay, funcToExec) {
   setTimeout(funcToExec, delay );
 }
@@ -8,19 +10,24 @@ function plainLog() {
 
 delayed(150, plainLog);
 
+/* 2 */
+
 let lines = 1;
 
 function printComment(comment, line = lines++) {
-    console.log(line, comment);
-    line++;
+  console.log(line, comment);
+  line++;
 }
 
 printComment('Oh yes');
+printComment('And even more so now');
+
+/* 3 */
 
 function argList(productName, price = 100) {
-    console.log( arguments.length );
-    console.log( productName === arguments[0] );
-    console.log( price === arguments[1] );
+  console.log( arguments.length );
+  console.log( productName === arguments[0] );
+  console.log( price === arguments[1] );
 }
 
 argList('Krill Oil Capsules');
